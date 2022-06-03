503127856cda4de1288ca50b90d104fda4c0f6ed
Chibueze <ogbuaguchibby17@gmail.com>
2022-06-03 16:35:51 -0700

    3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * @0 - Return value
+ * Return: Always 0
+ *
+ */
+
+int main(void)
+{
+	char low;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(low);
+	for (low = 'A'; low <= 'Z'; low++)
+		putchar(low);
+	putchar('\n');
+
+	return (0);
+}
