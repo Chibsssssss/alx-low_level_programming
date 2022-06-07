9c03c2f220fc7c2c77e7d12e2720cd54e1990be9
Chibueze <ogbuaguchibby17@gmail.com>
2022-06-07 16:40:38 -0700

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <main.h>
+
+/**
+ * main - Entry point
+ * @0 - Return value
+ * Return: Anything
+ *
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	       printf(i);
+	return (0);
+}
