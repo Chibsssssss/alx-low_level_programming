ca3eadc953738478584f513542467673042bb1af
Chibsssssss <ogbuaguchibby17@gmail.com>
2022-06-10 01:08:23 +0100

    2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
+ * main - check the code for  School students.
  *
  * Return: Always 0.
  */
-int main(void)
+int mul(int a, int b);
 {
   printf("%d\n", mul(98, 1024));
   printf("%d\n", mul(-402, 4096));
